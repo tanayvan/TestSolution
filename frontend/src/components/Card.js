87e6b1d093ad168e956376ec7629c99a910d49aa
Tanay Van <tanayvan258@gmail.com>
2020-09-15 12:08:08 +0530

    added hover animation

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,10 +1,24 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useSpring, animated, config } from "react-spring";
+
 import { addtoCart, getCart } from "../apicalls/apicalls";
 import CartContext from "../context/CartContext";
 
+const calc = (x, y) => [
+  -(y - window.innerHeight / 2) / 20,
+  (x - window.innerWidth / 2) / 20,
+  1.1,
+];
+const trans = (x, y, s) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
 export default function Card({ data }) {
   const { cart, setCart } = useContext(CartContext);
+  const [props, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: config.wobbly,
+  }));
 
   const checkProduct = () => {
     const obj = cart.find((obj) => obj.product._id == data._id);
@@ -38,33 +52,31 @@ export default function Card({ data }) {
   };
 
   return (
-    <div>
-      <div
-        class="card my-5"
-        style={{
-          width: "18rem",
-        }}
-      >
-        <img
-          src={require(`../assests/${data.image}`)}
-          class="card-img-top"
-          alt="..."
-        />
-        <div class="card-body">
-          <h5 class="card-title">{data.name}</h5>
-          <p class="card-text">{data.description}</p>
-          <p class="card-text">{`${data.price} INR`}</p>
-          {checkProduct() ? (
-            <Link class="btn btn-primary" to="/cart">
-              View In Cart
-            </Link>
-          ) : (
-            <button class="btn btn-primary" onClick={handleClick}>
-              Add to Cart
-            </button>
-          )}
-        </div>
+    <animated.div
+      class="card my-5"
+      style={{ transform: props.xys.interpolate(trans), width: "18rem" }}
+      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+      onMouseLeave={() => set({ xys: [0, 0, 1] })}
+    >
+      <img
+        src={require(`../assests/${data.image}`)}
+        class="card-img-top"
+        alt="..."
+      />
+      <div class="card-body">
+        <h5 class="card-title">{data.name}</h5>
+        <p class="card-text">{data.description}</p>
+        <p class="card-text">{`${data.price} INR`}</p>
+        {checkProduct() ? (
+          <Link class="btn btn-primary" to="/cart">
+            View In Cart
+          </Link>
+        ) : (
+          <button class="btn btn-primary" onClick={handleClick}>
+            Add to Cart
+          </button>
+        )}
       </div>
-    </div>
+    </animated.div>
   );
 }
